ccd373e95058dbe018ef9db377b2c67029fedd97
paremeter373 <105030995+paremeter373@users.noreply.github.com>
2024-08-17 10:47:43 -0400

    home page
    Coce for home page

--- Frontend/pages.js
+++ Frontend/pages.js
@@ -2,13 +2,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        // Get the target element
         const target = document.querySelector(this.getAttribute('href'));
 
-        // Get the target element's top position and subtract to show the gap
-        const topPosition = target.getBoundingClientRect().top + window.pageYOffset - 200; // Adjust 50px to your needs
+        const topPosition = target.getBoundingClientRect().top + window.pageYOffset - 200; 
 
-        // Smooth scroll to the adjusted position
         window.scrollTo({
             top: topPosition,
             behavior: 'smooth'
